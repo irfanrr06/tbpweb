0975c1cffaf27c4101d16b68e1e8860c52547e03
rivaldo120899 <60865578+rivaldo120899@users.noreply.github.com>
2020-06-10 15:32:48 +0700

    Alasan Pembatalan KP

--- resources/views/klp04/cancellations/_form.blade.php
+++ resources/views/klp04/cancellations/_form.blade.php
@@ -1,8 +1,16 @@
 <div class="form-group">
+
+
     <label class="form-label" for="field">Alasan Pembatalan KP</label>
+
     {{ html()->textarea('field')->class(['form-control', 'is-invalid' 
+
     => $errors->has('field')])->id('field')}}
+
     @error('field')
+
     <div class="invalid-feedback">{{ $errors->first('field') }}</div>
+
     @enderror
+    
 </div>
